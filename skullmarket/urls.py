d9d3acffc062cb7b6bf18602ba4b5862bda3fb0e
Allan Rouet-Alvaro <allan.rouet@gmail.com>
2025-05-11 18:28:01 +0200

    fix: corrige render.yaml

--- skullmarket/urls.py
+++ skullmarket/urls.py
@@ -35,3 +35,7 @@ urlpatterns += [
         'document_root': settings.MEDIA_ROOT,
     }),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
